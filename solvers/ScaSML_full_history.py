df54b74e2f6792685313c907ba47ee84dc74c390
Francis-Fan-create <145922867+Francis-Fan-create@users.noreply.github.com>
2025-01-12 14:16:43 +0800

    Update uncertainty

--- solvers/ScaSML_full_history.py
+++ solvers/ScaSML_full_history.py
@@ -249,6 +249,6 @@ class ScaSML_full_history(object):
         u_hat = self.GP.predict(x_t)
         
         # Calculate and return the final u value, if |u_breve|<sqrt(compute_PDE_loss(x_t)), return u_breve+u_hat, else return u_hat
-        uncertainty = jnp.sqrt(self.GP.compute_PDE_loss(x_t))
+        uncertainty = jnp.abs(self.GP.compute_PDE_loss(x_t))
         u = jnp.where(jnp.abs(u_breve) < uncertainty, u_breve + u_hat, u_hat)
         return u
