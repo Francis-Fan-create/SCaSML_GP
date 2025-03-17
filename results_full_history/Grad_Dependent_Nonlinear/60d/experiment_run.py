2d179c988f86ef4c343a5843eb7aa43a730d9ede
Francis-Fan-Create <2200010816@stu.pku.edu.cn>
2025-03-17 11:47:34 +0800

    high dim update

--- results_full_history/Grad_Dependent_Nonlinear/60d/experiment_run.py
+++ results_full_history/Grad_Dependent_Nonlinear/60d/experiment_run.py
@@ -61,12 +61,12 @@ solver3_2=ScaSML_full_history(equation=equation,GP=solver1_2) #ScaSML object
 solver3_3=ScaSML_full_history(equation=equation,GP=solver1_3) #ScaSML object
 
 
-#run the test for SimpleUniform
-test2=SimpleUniform(equation,solver1_1,solver2,solver3_1)
-test2.test(r"results_full_history/Grad_Dependent_Nonlinear/60d")
-#run the test for ConvergenceRate
-test3=ConvergenceRate(equation,solver1_2,solver2,solver3_2)
-test3.test(r"results_full_history/Grad_Dependent_Nonlinear/60d")
+# #run the test for SimpleUniform
+# test2=SimpleUniform(equation,solver1_1,solver2,solver3_1)
+# test2.test(r"results_full_history/Grad_Dependent_Nonlinear/60d")
+# #run the test for ConvergenceRate
+# test3=ConvergenceRate(equation,solver1_2,solver2,solver3_2)
+# test3.test(r"results_full_history/Grad_Dependent_Nonlinear/60d")
 #run the test for InferenceScaling
 test4=InferenceScaling(equation,solver1_3,solver2,solver3_3)
 test4.test(r"results_full_history/Grad_Dependent_Nonlinear/60d")
